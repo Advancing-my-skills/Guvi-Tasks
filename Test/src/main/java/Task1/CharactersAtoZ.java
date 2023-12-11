09ddd8b9364adf23e828955974f21abdb067be5b
Vinetha <149235882+Advancing-my-skills@users.noreply.github.com>
2023-12-11 22:59:55 +0530

    Task

---
+++ Test/src/main/java/Task1/CharactersAtoZ.java
@@ -0,0 +1,13 @@
+package Task1;
+
+public class CharactersAtoZ {
+  public static void main(String[] args) {
+	  char start = 'a';
+      char end = 'z';
+
+      for (char ch = start; ch <= end; ch++) {
+          System.out.print(ch + " ");
+      }
+ 
+  }
+}
